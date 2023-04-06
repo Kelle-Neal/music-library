b3e47d354668b5915f8a037c1315c90bb00025e7
Kelle-Neal <kelle.neal@student.awesomeinc.org>
2023-04-06 19:32:33 +0000

    Data

--- library/myMusic/models.py
+++ library/myMusic/models.py
@@ -3,45 +3,45 @@ from django.db import models
 # Create your models here.
 class Artist(models.Model):
   name = models.CharField(max_length=300)
-  bio = models.TextField(null=True)
-  image = models.URLField(null=True)
-  albums = models.ManyToManyField('Album')
+  bio = models.TextField(null=True, blank=True)
+  image = models.URLField(null=True, blank=True)
+  albums = models.ManyToManyField('Album', blank=True)
 
   def __str__(self):
     return f'{self.name}'
 
 class Album(models.Model):
   name = models.CharField(max_length=500)
-  artists= models.ForeignKey('Artist', on_delete=models.PROTECT, null=True)
-  releaseDate = models.DateField(null=True)
-  artwork = models.URLField(null=True)
-  genres = models.ManyToManyField('Genre')
-  songs = models.ManyToManyField('Song')
+  artists= models.ForeignKey('Artist', on_delete=models.PROTECT, null=True, blank=True)
+  releaseDate = models.DateField(null=True, blank=True)
+  artwork = models.URLField(null=True, blank=True)
+  genres = models.ManyToManyField('Genre', blank=True)
+  songs = models.ManyToManyField('Song', blank=True)
 
   def __str__(self):
     return f'{self.name}'
 
 class Song(models.Model):
   name = models.CharField(max_length=500)
-  artist = models.ForeignKey(Artist, on_delete=models.PROTECT, null=True)
-  albumName = models.ForeignKey(Album, on_delete=models.PROTECT, null=True)
+  artist = models.ForeignKey(Artist, on_delete=models.PROTECT, null=True, blank=True)
+  albumName = models.ForeignKey(Album, on_delete=models.PROTECT, null=True, blank=True)
 
   def __str__(self):
     return f'{self.name}'
 
 class Genre(models.Model):
   """Model representing the genre of a song or album."""
-  name = models.CharField(max_length=100)
+  name = models.CharField(max_length=100, blank=True)
 
   def __str__(self):
     return f'{self.name}'  
 
 class Playlist(models.Model):
   name = models.CharField(max_length=150)
-  description = models.TextField(null=True)
-  songs = models.ManyToManyField('Song')
-  albums = models.ManyToManyField('Album')
-  artists = models.ManyToManyField('Artist')
+  description = models.TextField(null=True, blank=True)
+  songs = models.ManyToManyField('Song', blank=True)
+  albums = models.ManyToManyField('Album', blank=True)
+  artists = models.ManyToManyField('Artist', blank=True)
 
   def __str__(self):
     return f'{self.name}'
