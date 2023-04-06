3e12b1c3cbfba89e9859e00028abac83930a0038
Kelle-Neal <kelle.neal@student.awesomeinc.org>
2023-04-06 18:38:10 +0000

    tables

--- library/myMusic/admin.py
+++ library/myMusic/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Artist, Album, Song, Genre
+from .models import Artist, Album, Song, Genre, Playlist
 # Register your models here.
 
 admin.site.register(Artist)
 admin.site.register(Album)
 admin.site.register(Song)
 admin.site.register(Genre)
-
+admin.site.register(Playlist)
--- library/myMusic/models.py
+++ library/myMusic/models.py
@@ -4,37 +4,56 @@ from django.db import models
 class Artist(models.Model):
   name = models.CharField(max_length=300)
   bio = models.TextField(null=True)
+  image = models.URLField(null=True)
   albums = models.ManyToManyField('Album')
 
-  # image = models.URLField(null=True)
-  # song = models.ForeignKey(
-  #   'Song', 
-  #   on_delete=models.PROTECT,
-  #   null=True
-  # )
+  def __str__(self):
+    return f'{self.name}'
 
 class Album(models.Model):
   name = models.CharField(max_length=500)
-  # artist = models.ForeignKey(
-  #   'Artist', 
-  #   on_delete=models.PROTECT,
-  #   null=True
-  # )
-
+  artists= models.ForeignKey('Artist', on_delete=models.PROTECT, null=True)
   releaseDate = models.DateField(null=True)
+  artwork = models.URLField(null=True)
+  genres = models.ManyToManyField('Genre')
   songs = models.ManyToManyField('Song')
-  # artwork = models.URLField(null=True)
+
+  def __str__(self):
+    return f'{self.name}'
 
 class Song(models.Model):
   name = models.CharField(max_length=500)
-  genres = models.ManyToManyField('Genre')
+  artist = models.ForeignKey(Artist, on_delete=models.PROTECT, null=True)
+  albumName = models.ForeignKey(Album, on_delete=models.PROTECT, null=True)
 
+  def __str__(self):
+    return f'{self.name}'
 
 class Genre(models.Model):
   """Model representing the genre of a song or album."""
   name = models.CharField(max_length=100)
 
-  # SO WE CAN SEE THE NAMES OF THE GENRES IN THE ADMIN PANEL
-  # def __str__(self):
-  #   return self.name
+  def __str__(self):
+    return f'{self.name}'  
 
+class Playlist(models.Model):
+  name = models.CharField(max_length=150)
+  description = models.TextField(null=True)
+  songs = models.ManyToManyField('Song')
+  albums = models.ManyToManyField('Album')
+  artists = models.ManyToManyField('Artist')
+
+  def __str__(self):
+    return f'{self.name}'
+
+
+
+
+
+
+
+
+
+
+
+# info_321
